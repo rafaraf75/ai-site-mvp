fd6f3952e9a0e589506a553fe43ae6d7b481222f
Rafal Labunski <rafallabunski@gmail.com>
2025-10-27 18:44:13 +0100

    feat(routing): redirects instead of middleware (+ fallback pages)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/pl',
+        permanent: false,
+      },
+      {
+        source: '/services',
+        destination: '/pl/services',
+        permanent: false,
+      },
+      {
+        source: '/contact',
+        destination: '/pl/contact',
+        permanent: false,
+      },
+      {
+        // redirect każdy path bez prefiksu języka na /pl/...
+        source: '/((?!pl|en|es).*)',
+        destination: '/pl/$1',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
